aeb2b95e817551ca677273b594dde4bb3b61ca96
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2019-11-08 13:46:59 -0800

    Initial revision

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages, Command
+
+
+def get_requirements(path):
+    content = read(path)
+    return [req for req in content.split("\n") if req != "" and not req.startswith("#")]
+
+
+setup_args = {
+    "name": "resgen-python",
+    "version": "0.1.0",
+    "packages": find_packages(),
+    "description": "Python bindings for the resgen genomic data service",
+    # "long_description": read("README.md"),
+    "long_description_content_type": "text/markdown",
+    "url": "https://github.com/reservoirgenomics/resgen-python",
+    "include_package_data": True,
+    "zip_safe": False,
+    "author": "Peter Kerpedjiev",
+    "author_email": "pkerpedjiev@gmail.com",
+    "keywords": ["resgen"],
+    "classifiers": [
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Multimedia :: Graphics",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    "install_requires": get_requirements("requirements.txt"),
+    "setup_requires": [],
+    "tests_require": ["pytest"],
+}
+
+setup(**setup_args)
