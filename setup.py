de9eb426010308f3f5fcf8f848432ea278c1c574
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2024-09-30 19:38:20 -0700

    Bump version: 0.6.2 → 0.6.3

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def get_requirements(path):
 
 setup_args = {
     "name": "resgen-python",
-    "version": "0.6.2",
+    "version": "0.6.3",
     "packages": find_packages(),
     "description": "Python bindings for the resgen genomic data service",
     # "long_description": read("README.md"),
