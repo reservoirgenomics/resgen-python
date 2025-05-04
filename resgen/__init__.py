9af0faee9aff7b459455da9e58789c2f4c4b45b7
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2025-05-04 09:42:16 -0700

    Bump version: 0.7.1 → 0.7.2

--- resgen/__init__.py
+++ resgen/__init__.py
@@ -28,7 +28,7 @@ from resgen.utils import tracktype_default_position, datatype_to_tracktype, infe
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-__version__ = "0.7.1"
+__version__ = "0.7.2"
 
 RESGEN_HOST = "https://resgen.io"
 RESGEN_BUCKET = "resgen"
--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def get_requirements(path):
 
 setup_args = {
     "name": "resgen-python",
-    "version": "0.7.1",
+    "version": "0.7.2",
     "packages": find_packages(),
     "description": "Python bindings for the resgen genomic data service",
     # "long_description": read("README.md"),
